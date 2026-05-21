0144bde68a0ec641b55e05f462cde3dd962f184f
learntospeak <227579325+learntospeak@users.noreply.github.com>
2026-05-22 07:10:21 +1000

    Load tree toolbox workflow upgrade

--- mascot.js
+++ mascot.js
@@ -59,6 +59,7 @@
       import("./windows-ping-fileserver-upgrade.js?v=20260521ping1").catch(() => {});
       import("./windows-taskkill-upgrade.js?v=20260521taskkill1").catch(() => {});
       import("./windows-attrib-upgrade.js?v=20260521attrib1").catch(() => {});
+      import("./windows-tree-upgrade.js?v=20260521tree1").catch(() => {});
     }, 0);
   }
 
