96ef22169e476bc9fd86562b8e01988478d39b33
learntospeak <227579325+learntospeak@users.noreply.github.com>
2026-05-22 21:20:26 +1000

    Load incident ahead fix

--- mascot.js
+++ mascot.js
@@ -68,6 +68,7 @@
       import("./final-polish-cisco.js?v=20260522ciscopolish1").catch(() => {});
       import("./final-polish-process.js?v=20260522processpolish1").catch(() => {});
       import("./final-polish-review.js?v=20260522reviewpolish1").catch(() => {});
+      import("./incident-ahead-fix.js?v=20260522ahead1").catch(() => {});
       import("./direct-scenario-router.js?v=20260521router4").catch(() => {});
       routeDirectScenarioNow();
     }, 0);
