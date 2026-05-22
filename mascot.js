eeff136eba720161c866b67cd90f8a9f72c43e6e
learntospeak <227579325+learntospeak@users.noreply.github.com>
2026-05-22 13:30:46 +1000

    Load Cisco batch 1 upgrade patch

--- mascot.js
+++ mascot.js
@@ -53,6 +53,7 @@
       import("./linux-batch1-upgrades.js?v=20260522linux1").catch(() => {});
       import("./linux-batch2-upgrades.js?v=20260522linux2").catch(() => {});
       import("./linux-batch3-upgrades.js?v=20260522linux3").catch(() => {});
+      import("./cisco-batch1-upgrades.js?v=20260522cisco1").catch(() => {});
       import("./direct-scenario-router.js?v=20260521router4").catch(() => {});
       routeDirectScenarioNow();
     }, 0);
