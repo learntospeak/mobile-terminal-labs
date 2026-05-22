d769779b0c9a4d24fd275c8f7f95bd72d46cc716
learntospeak <227579325+learntospeak@users.noreply.github.com>
2026-05-22 17:46:01 +1000

    Load security batch 3 upgrades

--- mascot.js
+++ mascot.js
@@ -57,6 +57,7 @@
       import("./cisco-batch2-upgrades.js?v=20260522cisco2").catch(() => {});
       import("./security-batch1-upgrades.js?v=20260522sec1").catch(() => {});
       import("./security-batch2-upgrades.js?v=20260522sec2").catch(() => {});
+      import("./security-batch3-upgrades.js?v=20260522sec3").catch(() => {});
       import("./direct-scenario-router.js?v=20260521router4").catch(() => {});
       routeDirectScenarioNow();
     }, 0);
